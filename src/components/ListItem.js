322787220bfc85e2c8327443ebc6c0998a820697
John Herman Witham <john.h.witham@gmail.com>
2017-05-31 23:03:16 +0300

    created action creator and wired it

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
-
     const { titleStyle } = styles;
 
     return (
@@ -24,4 +25,4 @@ const styles = {
   }
 };
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
